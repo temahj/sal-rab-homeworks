c540cf95c2d145cab3775c4ba1170ef155f3c089
hyipoimiikto <artyom.zorin2013@gmail.com>
2023-01-21 10:23:47 +0300

    task №1 done fixed

--- task_1/solution.js
+++ task_1/solution.js
@@ -2,7 +2,7 @@
 function addInCartMessage(title, price) {
     let productName = title;
     let productPrice = price;
-    let message = productName + ' ' + 'за' + ' ' + productPrice + ' ' + 'теперь в корзине!';
+    let message = productName +  ' за ' + productPrice + ' теперь в корзине!';
     
     console.log(message);
 
