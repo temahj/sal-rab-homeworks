ffba1d04433dabdf5ce16ed4a3fc9c22b537a961
hyipoimiikto <artyom.zorin2013@gmail.com>
2023-01-23 09:49:44 +0300

    final task done

--- task_final/solution.js
+++ task_final/solution.js
@@ -1,19 +1,15 @@
 function sendRequest(name, phone, address, goods, sum) {
     let data = {client: {}, order: {}, goods: []};
     let countOfGoods = goods.length;
-    data.goods = [];
-    for (let i = 0; i <= countOfGoods; i++) {
-        if (typeof goods[i] == 'object' && goods[i] != null){
-            data.goods.push(goods[i]);
-            delete data.goods[i].id;
-            delete data.goods[i].price;
+    for (let i = 0; i < countOfGoods; i++) {
+        if (typeof goods[i] == 'object'){
+            data.goods.push({title: goods[i].title, count: goods[i].count});
         }
     }
 
     data.client = `${name} ${phone}`;
     data.order= {address: `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв. ${address.flat}`, sum: Number(sum)};
-    let file = {data: data};
-    let jsonData = JSON.stringify(file);
+    let jsonData = JSON.stringify({data});
 
     return jsonData;
 }
